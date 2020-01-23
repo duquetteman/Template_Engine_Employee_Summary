2759b52279051ac932b488641ca2c31cfcdf1b75
Patrick Duquette <duquetteman@gmail.com>
2020-01-22 18:53:50 -0600

    started work on the app.js file.

---
+++ app.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+const fs = require("fs");
+
